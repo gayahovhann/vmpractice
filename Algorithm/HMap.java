22be76c4aeb22fcbd196cc7a5e3b4e96533788d1
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2022-05-31 16:51:20 +0400

    hashMap

---
+++ Algorithm/HMap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class HMap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int c = 35;
+        HashMap<String, Integer> hafhaf = new HashMap<String, Integer>();
+        hafhaf.put("a", 10);
+        hafhaf.put("b", b);
+        hafhaf.put("c", 35);
+
+        System.out.println(hafhaf);
+
+    }
+}
